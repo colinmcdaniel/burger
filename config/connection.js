e1ebb65a59632c0a597d8b9d789bd25318d260dd
Colin McDaniel <colinmcdaniel@me.com>
2016-10-03 14:53:19 -0700

    Add JawsDB connection dependency

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port:3306,
-	host: 'localhost',
-	user: 'root',
-	password: 'drummrboy1',
-	database: 'burgers_db'
-});
+var connection;
+
+if(process.env.JAWSDB_URL)
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+else{
+	var connection = mysql.createConnection({
+		port:3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'drummrboy1',
+		database: 'burgers_db'
+	});
+}
 
 connection.connect(function (err) {
 	if (err) {
